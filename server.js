14b18562d43ce361c842ea8c988b8d433e179db5
umlspec <umlspec@hotmail.com>
2018-04-06 14:40:50 +0100

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials');
 var app = express();
 app.set('view engine', 'hbs');
@@ -64,7 +66,7 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 });
 
